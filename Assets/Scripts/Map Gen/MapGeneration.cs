10ac77a744537a7157b91801c3f57e0a995a662e
RVKE <jurrejacob.devries@coderclass.nl>
2019-11-15 21:21:32 +0100

    Working on Tileset placement system

--- Assets/Scripts/Map Gen/MapGeneration.cs
+++ Assets/Scripts/Map Gen/MapGeneration.cs
@@ -9,33 +9,36 @@ public class MapGeneration : MonoBehaviour {
     public GameObject viewPoint;
     public GameObject mapParent;
 
-    //public GameObject tileSetObject;
+    Vector3 playerPos;
+    Vector3 prevPlayerPos;
 
-    //public Dictionary<Vector3, TileSet> tileSetDict;
+    [Range(1f, 4f)]
+    public int renderDist;
 
     public List<TileSet> tileSets;
 
-	void Start () {
-        
-	}
-	
 	void Update () {
         LoadTileSets();
 	}
     
     void LoadTileSets()
     {
-        //check if camera is looking at new tile position
-        int posX = (int)viewPoint.transform.position.x;
-        int posZ = (int)viewPoint.transform.position.z;
-
-        Vector3 pos = new Vector3(posX, 0, posZ);
-        Vector3 newPos = RoundVector(pos, tileSetSize);
-
-        if ()
-
-        //Debug.Log("rounded to ten: " + newPos + " normal: " + pos);
+        playerPos = viewPoint.transform.position;
+        
+        if (prevPlayerPos != playerPos)
+        {
+            for (int x = -renderDist * tileSetSize; x < renderDist * tileSetSize; x += tileSetSize)
+            {
+                for (int z = -renderDist * tileSetSize; z < renderDist * tileSetSize; z += tileSetSize)
+                {
+                    Vector3 newPos = new Vector3(x, 0, z);
+                    Debug.Log(newPos);
+                }
+            }
+            prevPlayerPos = playerPos;
+        }
     }
+
 
     public static Vector3 RoundVector(Vector3 vector, float roundSize)
     {
