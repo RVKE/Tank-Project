4fe77c073278ecf08a4c9530956ec67bfd912cd6
RVKE <jurrejacob.devries@coderclass.nl>
2019-11-17 18:05:25 +0100

    Working on Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,20 +12,33 @@ public class Player : MonoBehaviour {
 
     public PlayerState currentState;
 
+    public float speed;
+    public float rotateSpeed;
+
 	void Start () {
-		
-	}
+        currentState = PlayerState.Outside;
+    }
 	
 	void Update () {
 
-        /*if (currentState == PlayerState.Inside) {
-            Debug.Log("inside");
-        } */
+        if (currentState == PlayerState.Outside) {
+            PlayerMovement();
+        } 
 
         if (Input.GetKeyDown(KeyCode.Tab)) {
             UpdatePlayerState();
         }
 	}
+
+    void PlayerMovement()
+    {
+        var transAmount = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        var rotateAmount = rotateSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+
+        transform.Translate(0, 0, transAmount);
+        transform.Rotate(0, rotateAmount, 0);
+
+    }
 
 
     void UpdatePlayerState()
--- Assets/Scripts/ViewMovement.cs
+++ Assets/Scripts/ViewMovement.cs
@@ -1,14 +1,27 @@
 ﻿using UnityEngine;
 
-public class ViewMovement : MonoBehaviour
-{
-    public float speed = 10.0f;
+public class ViewMovement : MonoBehaviour {
+
+    private GameObject camObject;
+    private Camera cam;
+    private GameObject player;
+
+    void Start()
+    {
+        camObject = Camera.main.gameObject;
+        cam = Camera.main;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     void Update()
     {
-        //transform.Translate(Vector3.forward * speed);
-        float translation = (Input.GetAxis("Vertical")) * speed * Time.deltaTime;
-        float straffe = (Input.GetAxis("Horizontal")) * speed * Time.deltaTime;
+        transform.position = player.transform.position;
+    }
+
+    void DebugMovement()
+    {
+        float translation = (Input.GetAxis("Vertical")) * 10.0f * Time.deltaTime;
+        float straffe = (Input.GetAxis("Horizontal")) * 10.0f * Time.deltaTime;
 
         transform.Translate(straffe, 0, translation);
     }
