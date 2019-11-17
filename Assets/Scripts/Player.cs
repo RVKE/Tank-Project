094df321e42f0027de06cef873cd2241dfccaa17
RVKE <jurrejacob.devries@coderclass.nl>
2019-11-17 11:24:37 +0100

    Working on PlayerStates

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,13 +10,39 @@ public enum PlayerState
 
 public class Player : MonoBehaviour {
 
-    private PlayerState state;
+    public PlayerState currentState;
 
 	void Start () {
 		
 	}
 	
 	void Update () {
-		
+
+        /*if (currentState == PlayerState.Inside) {
+            Debug.Log("inside");
+        } */
+
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            UpdatePlayerState();
+        }
 	}
+
+
+    void UpdatePlayerState()
+    {
+        if (currentState == PlayerState.Inside)
+            currentState = PlayerState.Outside;
+        else
+            currentState = PlayerState.Inside;
+
+        switch (currentState)
+        {
+            case PlayerState.Inside:
+                Debug.Log("inside mode");
+                break;
+            case PlayerState.Outside:
+                Debug.Log("outside mode");
+                break;
+        }
+    }
 }
